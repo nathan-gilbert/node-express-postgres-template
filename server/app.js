ae04c92cb410390b554aba7a140948534f8d90a3
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-06-17 11:01:10 -0600

    reorg structure

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const http = require("http");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
-const db = require("./queries");
+const db = require("../database/queries");
 
 // Set up the express app
 const app = express();
