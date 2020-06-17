344f0565688e4be5e4bb3125f9bd6a5890a0cb6b
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-06-17 11:52:42 -0600

    add config packager

--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
+const config = require("config");
+
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "",
-  host: "localhost",
-  database: "node-demo",
-  password: "",
-  port: 5432,
+  user: config.get("user"),
+  host: config.get("host"),
+  database: config.get("database"),
+  password: config.get("password"),
+  port: config.get("db-port"),
 });
 
 function getUsers(_, response) {
