cfb3d68756cd4be115b9b5f2e6de6b58f44eed3c
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-06-23 16:29:58 -0600

    trying some routing options

--- server/__tests__/index.tests.js
+++ server/__tests__/index.tests.js
@@ -1,10 +1,15 @@
 const request = require("supertest");
-const app = require("..");
+const app = require("../index");
 
 describe("Index endpoint", () => {
   it("should return template text", async () => {
     const res = await request(app).get("/api");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty("info");
+  });
+
+  it("should return all users", async () => {
+    const res = await request(app).get("/api/users");
+    expect(res.statusCode).toEqual(200);
   });
 });
--- server/__tests__/users.tests.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("..");
-
-describe("Users endpoint", () => {
-  it("should return all users", async () => {
-    const res = await request(app).get("/api/users");
-    expect(res.statusCode).toEqual(200);
-  });
-});
