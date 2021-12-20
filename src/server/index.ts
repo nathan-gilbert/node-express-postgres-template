1eed2513e044664d6a69e41401760c423d42ff32
Nathan Gilbert <nathan.gilbert@gmail.com>
2021-12-19 17:00:21 -0700

    fix index lint errors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,25 +1,25 @@
-const express = require("express");
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-const db = require("../database");
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const db = require('../database')
 
-const app = express();
+const app = express()
 
 // Log requests to the console.
-app.use(logger("dev"));
+app.use(logger('dev'))
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/api", (_, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
+app.get('/api', (_, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
 
-app.get("/api/users", db.getUsers);
-app.get("/api/users/:id", db.getUserById);
-app.post("/api/users", db.createUser);
-app.put("/api/users/:id", db.updateUser);
-app.delete("/api/users/:id", db.deleteUser);
+app.get('/api/users', db.getUsers)
+app.get('/api/users/:id', db.getUserById)
+app.post('/api/users', db.createUser)
+app.put('/api/users/:id', db.updateUser)
+app.delete('/api/users/:id', db.deleteUser)
 
-module.exports = app;
+module.exports = app
